41767750e63f1913d1dac680c891d26e281321b8
Lahiru <devindalahiru2004@gmail.com>
2026-06-16 17:44:24 +0530

    refactor: improve error handling and add CRUD operations for students

--- BACKEND/routes/student.js
+++ BACKEND/routes/student.js
@@ -15,7 +15,7 @@ router.route('/add').post((req,res) => {
     newStuden.save().then(() => {
         res.json('Student added');
     }).catch((err) => {
-        console.log(err);
+        console.error(err.message);
         res.status(500).json('Error adding student');
     })
 });
@@ -24,10 +24,50 @@ router.route('/').get((req,res) => {
     Students.find().then((students) => {
         res.json(students);
     }).catch((err) => {
-        console.log(err);
+        console.error(err.message);
         res.status(500).json('Error fetching students');
     });
 });
+
+router.route('/update/:id').put(async (req,res) => {
+    const id = req.params.id;
+    const {name, age, gender} = req.body;
+
+    const updateStudent = {
+        name,
+        age,
+        gender
+    }
+
+    const update = await Student.findByIdAndUpdate(id, updateStudent).then(() => {
+        res.status(200).json('Student updated');
+    }).catch((err) => {
+        console.error(err.message);
+        res.status(500).json('Error updating student');
+    });
+});
+
+router.route('/delete/:id').delete(async (req,res) => {
+    const id = req.params.id;
+
+    await Student.findByIdAndDelete(id).then(() => {
+        res.status(200).json('Student deleted');
+    }).catch((err) => {
+        console.error(err.message);
+        res.status(500).json('Error deleting student');
+    });
+});
+
+router.route('/get/:id').get(async (req,res) => {
+    const id = req.params.id;
+
+    const filterStudent = await Student.findById(id).then((student) => {
+        res.status(200).json(student).send({ student : filterStudent });
+    }).catch((err) => {
+        console.error(err.message);
+        res.status(500).json('Error fetching student');
+    });
+});    
 
 
 module.exports = router;
